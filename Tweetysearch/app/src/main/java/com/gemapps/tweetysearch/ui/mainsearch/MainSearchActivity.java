4ff109d327c803c6bb0000ba376c60989b360c14
edug <edu@funler.com>
2017-02-12 20:02:54 -0300

    start with a basic search UI

---
+++ Tweetysearch/app/src/main/java/com/gemapps/tweetysearch/ui/mainsearch/MainSearchActivity.java
@@ -0,0 +1,46 @@
+/*
+ *    Copyright {yyyy} {name of copyright owner}
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.gemapps.tweetysearch.ui.mainsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gemapps.tweetysearch.R;
+
+public class MainSearchActivity extends AppCompatActivity {
+
+    private static final String SEARCH_FRAGMENT_TAG = "tweety.SEARCH_FRAGMENT_TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_search);
+        addSearchContent(savedInstanceState);
+    }
+
+    private void addSearchContent(Bundle savedInstanceState){
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.main_fragment_container,
+                            MainSearchFragment.newInstance(),
+                            SEARCH_FRAGMENT_TAG)
+                    .commit();
+        }
+    }
+}
